c6d33867c94a3318601493cb8d1eddd4cb50815e
Alison003 <59594478+Alison003@users.noreply.github.com>
2023-02-10 15:00:16 -0800

    menu & subBytes

---
+++ cmake-build-debug/CMakeFiles/AES.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "AES.exe"
+  "AES.exe.manifest"
+  "AES.pdb"
+  "CMakeFiles/AES.dir/main.cpp.obj"
+  "CMakeFiles/AES.dir/main.cpp.obj.d"
+  "libAES.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AES.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,20 +1,136 @@
 #include <iostream>
+#include <sstream>
+
+using namespace std;
+
+//message to be encrypted/decrypted
+string message [4][4];
+//key is: multiprogramming
+string key [4][4] = {{"6d","75","6c","74"},
+                     {"69","70","72","6f"},
+                     {"67","72","61","6d"},
+                     {"6d","69","6e","67"}};
+string sBox [16][16] = {{"63","7c","77","7b","f2","6b","6f","c5","30","01","67","2b","fe","d7","ab","76"},
+                        {"ca","82","c9","7d","fa","59","47","f0","ad","d4","a2","af","9c","a4","72","c0"},
+                        {"b7","fd","93","26","36","3f","f7","cc","34","a5","e5","f1","71","d8","31","15"},
+                        {"04","c7","23","c3","18","96","05","9a","07","12","80","e2","eb","27","b2","75"},
+                        {"09","83","2c","1a","1b","6e","5a","a0","52","3b","d6","b3","29","e3","2f","84"},
+                        {"53","d1","00","ed","20","fc","b1","5b","6a","cb","be","39","4a","4c","58","cf"},
+                        {"d0","ef","aa","fb","43","4d","33","85","45","f9","02","7f","50","3c","9f","a8"},
+                        {"51","a3","40","8f","92","9d","38","f5","bc","b6","da","21","10","ff","f3","d2"},
+                        {"cd","0c","13","ec","5f","97","44","17","c4","a7","7e","3d","64","5d","19","73"},
+                        {"60","81","4f","dc","22","2a","90","88","46","ee","b8","14","de","5e","0b","db"},
+                        {"e0","32","3a","0a","49","06","24","5c","c2","d3","ac","62","91","95","e4","79"},
+                        {"e7","c8","37","6d","8d","d5","4e","a9","6c","56","f4","ea","65","7a","ae","08"},
+                        {"ba","78","25","2e","1c","a6","b4","c6","e8","dd","74","1f","4b","bd","8b","8a"},
+                        {"70","3e","b5","66","48","03","f6","0e","61","35","57","b9","86","c1","1d","9e"},
+                        {"e1","f8","98","11","69","d9","8e","94","9b","1e","87","e9","ce","55","28","df"},
+                        {"8c","a1","89","0d","bf","e6","42","68","41","99","2d","0f","b0","54","bb","16"}};
+
+void getInput();
+void getMessage();
+void keyExpansion();
+void Encrypt();
+void subBytes();
+void shiftRows();
+void mixColumns();
+void roundKey();
+
+void menu(){
+    cout << "Please enter the number for the action you would like to perform\n";
+    cout << "1. Encrypt a message\n";
+    cout << "2. Decrypt a message\n";
+
+    int option;
+    cin >> option;
+
+    if(option == 1){
+        getInput();
+    }else if(option == 2){
+        getMessage();
+    }else{
+        cout << "That was not an option.\n";
+    }
+}
+
+//Gets message to be decrypted
+void getInput(){
+
+}
+
+//Gets message to be encrypted
+void getMessage() {
+    cout << "What message would you like to encrypt? (Please enter a 16 character message)\n";
+    string userInput;
+    cin >> userInput;
+
+    stringstream ss;
+
+    int index = 0;
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
+            ss << hex << int(userInput.at(index));
+            message[j][i] = ss.str();;
+            index++;
+        }
+    }
+    keyExpansion();
+    Encrypt();
+}
+
+void keyExpansion() {
+
+}
+
+void roundKey(){
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
+            message[j][i] = message[j][i] + key[j][i];
+        }
+    }
+}
+
+void subBytes() {
+    stringstream stream;
+    int first;
+    int second;
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
+            int x = message[j][i].at(0);
+            stream << x;
+            stream >> std::hex >> first;
+            int y = message[j][i].at(1);
+            stream << y;
+            stream >> std::hex >> second;
+            message[j][i] = sBox[first][second];
+        }
+    }
+}
+
+void shiftRows() {
+
+}
+
+void mixColumns() {
+
+}
+
+void Encrypt(){
+    roundKey();
+
+    for (int i = 0; i < 9; i++){
+        subBytes();
+        shiftRows();
+        mixColumns();
+        roundKey();
+    }
+
+    subBytes();
+    shiftRows();
+    roundKey();
+}
 
 int main() {
-    //1. Divide data into blocks
-
-    //2. Key expansion
-
-    //3. Adding round key
-
-    //4. Byte substitution
-
-    //5. Shifting rows
-
-    //6. Mixing columns
-
-    //7. Adding round key
-
-    //8. Repeat
+    menu();
     return 0;
 }

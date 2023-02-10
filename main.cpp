cfe0104a946bd69eaa5a988df4c141946e2c91c9
Alison003 <59594478+Alison003@users.noreply.github.com>
2023-02-09 23:27:54 -0800

    initial setup

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    //1. Divide data into blocks
+
+    //2. Key expansion
+
+    //3. Adding round key
+
+    //4. Byte substitution
+
+    //5. Shifting rows
+
+    //6. Mixing columns
+
+    //7. Adding round key
+
+    //8. Repeat
+    return 0;
+}

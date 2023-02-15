0667947ddb94fd3b2aef4da22308a7d03a5417cb
Alison003 <59594478+Alison003@users.noreply.github.com>
2023-02-15 14:44:51 -0800

    invSubBytes

--- main.cpp
+++ main.cpp
@@ -17,6 +17,11 @@ int matrix [4][4] = {{2,3,1,1},
                      {1,1,2,3},
                      {3,1,1,2}};
 
+int invMatrix [4][4] = {{-4/35, 3/35, -11/35, 17/35},
+                        {17/35, -4/35, 3/35, -11/35},
+                        {-11/35, 17/35, -4/35, 3/35},
+                        {3/35, -11/35, 17/35, -4/35}};
+
 int rCon [4][10] = {{1,2,4,8,10,20,40,80,27,36},
                    {0,0,0,0,0,0,0,0,0,0},
                    {0,0,0,0,0,0,0,0,0,0},
@@ -38,6 +43,23 @@ string sBox [16][16] = {{"63","7c","77","7b","f2","6b","6f","c5","30","01","67",
                         {"70","3e","b5","66","48","03","f6","0e","61","35","57","b9","86","c1","1d","9e"},
                         {"e1","f8","98","11","69","d9","8e","94","9b","1e","87","e9","ce","55","28","df"},
                         {"8c","a1","89","0d","bf","e6","42","68","41","99","2d","0f","b0","54","bb","16"}};
+
+string invSBox [16][16] = {{"52", "09", "6a", "d5", "30", "36", "a5", "38", "bf", "40", "a3", "9e", "81", "f3", "d7", "fb"},
+                           {"7c", "e3", "39", "82", "9b", "2f", "ff", "87", "34", "8e", "43", "44", "c4", "de", "e9", "cb"},
+                           {"54", "7b", "94", "32", "a6", "c2", "23", "3d", "ee", "4c", "95", "0b", "42", "fa", "c3", "4e"},
+                           {"08", "2e", "a1", "66", "28", "d9", "24", "b2", "76", "5b", "a2", "49", "6d", "8b", "d1", "25"},
+                           {"72", "f8", "f6", "64", "86", "68", "98", "16", "d4", "a4", "5c", "cc", "5d", "65", "b6", "92"},
+                           {"6c", "70", "48", "50", "fd", "ed", "b9", "da", "5e", "15", "46", "57", "a7", "8d", "9d", "84"},
+                           {"90", "d8", "ab", "00", "8c", "bc", "d3", "0a", "f7", "e4", "58", "05", "b8", "b3", "45", "06"},
+                           {"d0", "2c", "1e", "8f", "ca", "3f", "0f", "02", "c1", "af", "bd", "03", "01", "13", "8a", "6b"},
+                           {"3a", "91", "11", "41", "4f", "67", "dc", "ea", "97", "f2", "cf", "ce", "f0", "b4", "e6", "73"},
+                           {"96", "ac", "74", "22", "e7", "ad", "35", "85", "e2", "f9", "37", "e8", "1c", "75", "df", "6e"},
+                           {"47", "f1", "1a", "71", "1d", "29", "c5", "89", "6f", "b7", "62", "0e", "aa", "18", "be", "1b"},
+                           {"fc", "56", "3e", "4b", "c6", "d2", "79", "20", "9a", "db", "c0", "fe", "78", "cd", "5a", "f4"},
+                           {"1f", "dd", "a8", "33", "88", "07", "c7", "31", "b1", "12", "10", "59", "27", "80", "ec", "5f"},
+                           {"60", "51", "7f", "a9", "19", "b5", "4a", "0d", "2d", "e5", "7a", "9f", "93", "c9", "9c", "ef"},
+                           {"a0", "e0", "3b", "4d", "ae", "2a", "f5", "b0", "c8", "eb", "bb", "3c", "83", "53", "99", "61"},
+                           {"17", "2b", "04", "7e", "ba", "77", "d6", "26", "e1", "69", "14", "63", "55", "21", "0c", "7d"}};
 
 string result_arr[4][1];
 
@@ -74,12 +96,33 @@ void menu(){
 
 //Gets message to be decrypted
 void getInput(){
-    //cout << "What message would you like to decrypt? (Please enter a 16 character message)\n";
-    //string userInput;
-    //cin >> userInput;
-    cout << "Looking for file input.txt to decrypt it... Please make sure it has a set of 16 chars\n";
+    cout << "Looking for file encrypted.txt to encrypt it... Please make sure it has a set of 16 chars\n";
 
-    //TODO: put encrypted message into 2D message array
+    fstream f;
+    string tmp;
+    stringstream ss;
+    int i = 0;
+    int x = 0;
+    f.open("encrypted.txt",ios::in);
+    if (f.is_open()){
+        while(getline(f, tmp) && i < 4){
+            for(char c : tmp){
+                if(c == ' '){
+                    continue;
+                }
+                else {
+                    ss << hex << int(c);
+                    message[x][i] = ss.str();
+                    ss.flush();
+                    x++;
+                }
+
+                if(x%3 == 0){
+                    i++;
+                }
+            }
+        }
+    }
     Decrypt();
 }
 
@@ -94,6 +137,14 @@ void Decrypt() {
     invShiftRows();
     invSubBytes();
     invRoundKey();
+
+    ofstream f;
+    f.open("decrypted.txt");
+    for (int row = 0; row < 4; row++){
+        for (int col = 0; col < 4; col++){
+            f << message[row][col];
+        }
+    }
 }
 
 void invShiftRows(){
@@ -125,25 +176,33 @@ void invShiftRows(){
     message[3][0] = temp3;
 }
 
-//Gets message to be encrypted
-void getMessage() {
-    /**
-    cout << "What message would you like to encrypt? (Please enter a 16 character message)\n";
-    string userInput;
-    cin >> userInput;
-
-    stringstream ss;
-
-    int index = 0;
+void invSubBytes() {
+    stringstream stream;
+    int first;
+    int second;
     for(int i = 0; i < 4; i++){
         for(int j = 0; j < 4; j++){
-            ss << hex << int(userInput.at(index));
-            message[j][i] = ss.str();;
-            index++;
+            int x = message[j][i].at(0);
+            stream << x;
+            stream >> hex >> first;
+            int y = message[j][i].at(1);
+            stream << y;
+            stream >> hex >> second;
+            message[j][i] = invSBox[first][second];
         }
     }
-     */
+}
 
+void invMixColumns() {
+    //TODO: inv matrix multiplication
+}
+
+void invRoundKey() {
+    //TODO: inv round key schedule
+}
+
+//Gets message to be encrypted
+void getMessage() {
     cout << "Looking for file input.txt to encrypt it... Please make sure it has a set of 16 chars\n";
 
     fstream f;

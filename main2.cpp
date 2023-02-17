51b566f09a94abb1e7e963b8d88e9d938fd5972c
Yarrabozaed <yarraz3010@gmail.com>
2023-02-17 14:18:04 -0800

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include <sstream>
 #include <fstream>
+#include <bitset>
+#include <string>
 
 using namespace std;
 
 //message to be encrypted/decrypted
-string message [4][4];
+string message;
+string key = "peter what goes down";
 
 void SubE();
 void SubD();
-void XORE();
+void XORE(const string& arr, const string& arr_1);
 void XORD();
+string to_binary(const string& s);
 
 void menu(){
     cout << "Please enter the number for the action you would like to perform\n";
@@ -19,6 +23,14 @@ void menu(){
 
     int option;
     cin >> option;
+
+    //Read from file
+    fstream f;
+    string temp;
+    f.open("input.txt");
+    if (f.is_open()){
+        getline(f, message);
+    }
 
     if(option == 1){//SUB
         cout << "Please enter the number for the action you would like to perform\n";
@@ -38,7 +50,7 @@ void menu(){
         cout << "2. Decryption\n";
         cin >> option;
         if (option == 1){
-            XORE();
+            XORE(message, key);
         }else if (option == 2){
             XORD();
         }else {
@@ -48,6 +60,31 @@ void menu(){
         cout << "That was not an option.\n";
     }
 }
+
+void XORE(const string& m, const string& k){
+    string result = "";
+    string b_m = to_binary(m);
+    string b_k = to_binary(k);
+
+    for(int x = 0; x < b_m.length(); x++) {
+        if (b_m[x] != b_k[x]) {
+            result[x] = '1';
+        } else {
+            result[x] = '0';
+        }
+    }
+}
+
+string to_binary(const string& s){
+    string tmp = "";
+    for(int i = 0; i < s.size(); i++){
+        bitset<4> b (s.c_str()[i]);
+        tmp += b.to_string();
+    }
+    return tmp;
+}
+
+
 
 int main() {
     menu();
